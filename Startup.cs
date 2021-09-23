5e8931946ebee89e002505cee3e301ef18acfde8
melonsound <ghost.134@hotmail.com>
2021-09-23 14:54:02 +0300

    feat: add dinner service

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,8 @@ namespace TgBotAspNet
             services.AddSingleton<IChatService, TelegramService>();
             services.AddBotCommands();
             services.AddHostedService<BotService>();
-            services.AddHostedService<NoSleepService>();
+            //services.AddHostedService<NoSleepService>();
+            services.AddHostedService<DinnerPollingService>();
         }
 
 
